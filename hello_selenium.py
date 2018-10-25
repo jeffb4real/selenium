e2fb817474da7a59739e45954ba5ef0413af4e92
jeffb4real <urieow@gmail.com>
2018-10-25 16:01:19 -0700

    Script to demo that chromedriver is configured properly. Also, current virtual environment package list (pip freeze --all)

---
+++ hello_selenium.py
@@ -0,0 +1,12 @@
+# https://sites.google.com/a/chromium.org/chromedriver/getting-started
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()  # Optional argument, if not specified will search PATH
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
